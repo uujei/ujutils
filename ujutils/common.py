b2aa1a7c45a89071f80dbde04dbf2492bf6e75bf
Woojin Cho <woojin.cho@gmail.com>
2021-08-10 21:57:35 +0900

    3rd commit

--- ujutils/common.py
+++ ujutils/common.py
@@ -1,268 +1,183 @@
 import os
 from datetime import datetime
-import glob
-import itertools
-import joblib
+from typing import Union
+
 import numpy as np
 import pandas as pd
-from typing import Union
-from collections import Counter
-from watchdog.utils.dirsnapshot import DirectorySnapshot, DirectorySnapshotDiff
-from InquirerPy import inquirer
-from InquirerPy.validator import PathValidator
-from rich.console import Console
-from rich.table import Table
+from watchdog.utils.dirsnapshot import DirectorySnapshot
+from natsort import natsorted
 
-from .helpers import _drop_root, _linl, _filter_files
-from .config import EXTS_IMAGE, EXTS_SIGNAL, EXTS_TEXT, _STYLE, CHECKBOX_STYLE
+from .helpers import _filter_files, _linl
 
 
 ################################################################
-# CLI helpers
+# is_ipython
 ################################################################
-# (helper) _user_select_extensions
-def _user_select_extensions():
-    """User select extensions
-
-    Returns:
-        [type]: [description]
-    """
-
-    '''User selects extensions
-    
-    Returns
-    -------
-    None or list
-        None means all extensions
-    '''
-    choices = {
-        'All': None,
-        'Image': EXTS_IMAGE,
-        'Signal/Audio': EXTS_SIGNAL,
-        'Text/Table': EXTS_TEXT,
-        'Manual select': '__manual'
-    }
-    
-    extensions = inquirer.select(
-        message="Select extensions to scan:",
-        choices=[{'name': k, 'value': v} for k, v in choices.items()],
-        default="All",
-        **_STYLE,
-    ).execute()
-
-    if extensions == '__manual':
-        extensions = inquirer.text(
-            message="Enter extensions (',' seperated):",
-        ).execute()
-        extensions = _linl(extensions, sep=',', strip='. ')
-
-    return extensions
-
-
-# (helper) _user_select_subdirs
-def _user_select_subdirs(files: list) -> list:
-    """User selects subdirs from checkbox
-
-    Args:
-        files (list): list of files
-
-    Returns:
-        list: list of selected subdirs
-    """
-    _dirs = Counter(
-        sorted([os.path.dirname(x) for x in files])
-    )
-
-    def q(_):
-        return [
-            {"name": f"{_drop_root(k)} ({v} files)", "value": k, "enabled": False} for k, v in _dirs.items()
-        ]
-    
-    return inquirer.checkbox(
-        message="Select sub-directories to include:",
-        choices=q,
-        transformer=lambda x: f"{[_.split(' ', 1)[0] for _ in x]}",
-        validate=lambda x: len(x) > 0,
-        invalid_message="Should be at least 1 selection",
-        instruction="(select at least 1, ctrl+C for quit)",
-        **CHECKBOX_STYLE,
-    ).execute()
-
-
-# (helper) _get_levs_from_hrchy
-def _user_set_hrchy(subdirs: list) -> list:
-    '''User set hierarchy levels
-    
-    Parameters
-    ----------
-    subdirs : list
-    
-    Returns
-    -------
-    list
-        list of hierarchy levels
-        
-    Example
-    -------
-    >>> subdirs = ['train/OK', 'train/NG', 'test/OK', 'test/NG']
-    >>> _user_set_hrchy(subdirs)
-    ? Enter column name for 'train, test': split
-    ? Enter column name for 'OK, NG': label
-    ['split', 'label']
-    '''
-    
-    subdirs = [_drop_root(_) for _ in subdirs]
-    
-    _transpose = itertools.zip_longest(*[_.split('/') for _ in subdirs])
-    _columns = [', '.join(sorted(filter(None, set(_)), reverse=True)) for _ in _transpose]
-    
-    hrchy = []
-    for _items in _columns:
-        hrchy.append(
-            inquirer.text(
-                message=f"Enter column name for '{_items}':",
-                **SELECT_SYMBOLS,
-            ).execute()
-        )
-        
-    return hrchy
-
-# (helper) _cli_preview_table
-def _cli_preview_table(df, title=None, tbl_no=None, n_head=10, n_tail=10, console=None):
-    
-    # table 
-    table = Table()
-    
-    # title
-    if title is not None:
-        table.title = title
-        table.title_justify = 'left'
-
-    # set columns
-    for c in df.columns:
-        table.add_column(c, justify='left', )
-
-    # add rows
-    if len(df) > (n_head + n_tail):
-        for idx in df.index[:n_head]:
-            table.add_row(*[str(_) for _ in df.loc[idx, :].values])
-        for _ in range(1):
-            table.add_row(*['...' for _ in df.columns])
-        for idx in df.index[-n_tail:]:
-            table.add_row(*[str(_) for _ in df.loc[idx, :].values])
-    else:
-        for idx in df.index:
-            table.add_row(*[str(_) for _ in df.loc[idx, :].values])
-
-    if console is None:
-        console = Console()
-    console.print(table)
-    
-    
-################################################################
-# Functions
-################################################################
-# (helper) _is_python
 def is_ipython() -> bool:
-    '''Check if script is executed in IPython.
-    
-    Returns
-    -------
-    bool
-       True if script is executed in IPython
-       
-    Example
-    -------
-    >>> is_ipython()
-    '''
+    """Check if script is executed in IPython (incl. Jupyter)
+
+    Examples:
+        >>> is_ipython()
+        True
+
+    Returns:
+        bool: [description]
+    """
+
     try:
         from IPython import get_ipython
-    except:
+    except BaseException:
         return False
+
     if get_ipython() is not None:
         return True
     return False
 
 
-# (helper) sorted
-def ml_sorted(x: list) -> list:
-    '''Sort list by machine learning convention
-    
-    Example
-    -------
-    >>> x = ['NG1', 'NG2', 'OK', 'NG2', 'NG3']
-    >>> _sorted(x)
-    ['OK', 'NG1', 'NG2', 'NG2', 'NG3']
-    >>> x = ['test', 'val', 'train']
-    ['train', 'test', 'val']
-    '''
-    FIRST = ['', '0', 'ok', 'train']
-    SECOND = ['val', 'validation', 'dev', 'develop', 'development']
+################################################################
+# mlsorted
+################################################################
+def mlsorted(x: list, order: list = None) -> list:
+    """Sort list by ML convention
 
-    return (
-        sorted([elem for elem in x if str(elem).lower() in FIRST])
-        + sorted([elem for elem in x if str(elem).lower() in SECOND])
-        + sorted([elem for elem in x if str(elem).lower() not in FIRST + SECOND])
-    )
+    Examples:
+        >>> x = ['NG1', 'NG2', 'OK', 'NG2', 'NG3']
+        >>> mlsorted(x)
+        ['OK', 'NG1', 'NG2', 'NG2', 'NG3']
+
+    Args:
+        x (list): List to sort.
+        order (list, optional): Give custom order. Defaults to None.
+
+    Returns:
+        list: Sorted list
+    """
+    # default order
+    if order is None:
+        order = [
+            ['train'],
+            ['val', 'valid', 'validation'],
+            ['dev', 'develop', 'deveopment'],
+            ['test'],
+            ['eval', 'evaluate', 'evaluation'],
+            ['ok', 'neg', 'nega', 'negative'],
+            ['ng', 'pos', 'posi', 'positive'],
+        ]
+
+    # generate map
+    _order = dict()
+    for i, _list in enumerate(order):
+        if not isinstance(_list, list):
+            _list = [_list]
+        _order.update(
+            {_elem: str(i) for _elem in _list}
+        )
+
+    # define translate
+    def _translate(x: str, _map: dict = _order):
+        y = x.lower()
+        for k, v in _map.items():
+            y = y.replace(k, v)
+        return y
+
+    return natsorted(x, key=lambda x: _translate(x))
 
 
-# df_files
-def df_files(
-    root: str=None,
-    snapshot: DirectorySnapshot=None,
-    hrchy: Union[str, list]=None, 
-    subdirs: Union[str, list]=None, 
-    extensions: Union[str, list]=None,
-    include_meta: bool=False,
-    ) -> pd.DataFrame:
-    '''Make dataframe of list of files
+################################################################
+# table_files
+################################################################
+def table_files(
+    root: str = None,
+    snapshot: DirectorySnapshot = None,
+    hrchy: Union[str, list] = None,
+    subdirs: Union[str, list] = None,
+    extensions: Union[str, list] = None,
+    include_meta: bool = False,
+) -> pd.DataFrame:
+    """Make stat table (filename, filepath, ...) for given root directory
 
-    Example
-    -------
-    >>> df = df_files('./dataset')
-    '''
-    assert (root is not None) or (snapshot is not None), "[EXIT] 'root' or 'files' should be given"
-    
+    Examples:
+        >>> df = table_files('./test_dataset')
+        >>> df = table_files('./test_dataset', hrchy='split/label')
+        >>> df = table_files(
+                './test_dataset',
+                hrchy='split/label',
+                subdirs=['train/NG', 'train/OK', 'test/NG', 'test/OK']
+            )
+
+    Args:
+        root (str, optional): Path of root directory. Defaults to None.
+        snapshot (DirectorySnapshot, optional):
+            Use only when this function used in other functions.
+        hrchy (Union[str, list], optional):
+            Name each directory depth(= column name).
+        subdirs (Union[str, list], optional):
+            Subdirectories to include. None means all.
+        extensions (Union[str, list], optional):
+            Extensions to include. None means all.
+        include_meta (bool, optional):
+            Include metadata (size, modified datetime) or not.
+
+    Returns:
+        pd.DataFrame
+    """
+
+    assert (root is not None) or (
+        snapshot is not None
+    ), "[EXIT] 'root' or 'files' should be given"
+
     # temporary field
-    TEMP_FIELD = '__label'
+    TEMP_FIELD = "__label"
 
     # correct inputs
     root = os.path.relpath(root)
     if hrchy is not None:
-        hrchy = _linl(hrchy, sep='/')
+        hrchy = _linl(hrchy, sep="/")
     if extensions is not None:
-        extensions = _linl(extensions, sep=',')
+        extensions = _linl(extensions, sep=",")
 
     # take a snapshot
     if snapshot is None:
         snapshot = DirectorySnapshot(root)
-    files = sorted([fp for fp in snapshot.paths if os.path.isfile(fp)])
+    files = [fp for fp in snapshot.paths if os.path.isfile(fp)]
 
+    # filter by extensions
     if extensions is not None:
         files = _filter_files(files, extensions=extensions)
 
+    # filter by subdirs
     if subdirs is not None:
         files = _filter_files(files, subdirs=subdirs)
-        
+
+    # sort list
+    files = mlsorted(files)
+
     # create table
-    df = pd.DataFrame({
-        '__label': [x.rsplit('/', 1)[0] for x in files],
-        'filename': [x.rsplit('/', 1)[-1] for x in files],
-        'filepath': files
-    })
-    
+    df = pd.DataFrame(
+        {
+            "__label": [x.rsplit("/", 1)[0] for x in files],
+            "filename": [x.rsplit("/", 1)[-1] for x in files],
+            "filepath": files,
+        }
+    )
+
     # concat meta (size, created)
     if include_meta:
-        _meta = pd.DataFrame({
-            'size': [snapshot._stat_info[fp].st_size for fp in files],
-            'modified': [datetime.fromtimestamp(snapshot._stat_info[fp].st_mtime) for fp in files],
-        })
+        _meta = pd.DataFrame(
+            {
+                "size": [snapshot._stat_info[fp].st_size for fp in files],
+                "modified": [
+                    datetime.fromtimestamp(snapshot._stat_info[fp].st_mtime)
+                    for fp in files
+                ],
+            }
+        )
         df = pd.concat([df, _meta], axis=1)
-        
+
     # concat labels
-    labels = df[TEMP_FIELD].str.split('/', expand=True).iloc[:, 1:]
+    labels = df[TEMP_FIELD].str.split("/", expand=True).iloc[:, 1:]
     _ncols = labels.shape[1]
-    
+
     # set column names
     if hrchy is None:
         columns = [f"_lv{i+1}" for i in range(_ncols)]
@@ -271,68 +186,97 @@ def df_files(
     labels.columns = columns
 
     # concat labels and list of files
-    df = (
-        pd
-        .concat([labels, df.drop(columns=TEMP_FIELD)], axis=1)
-        .sort_values(by=columns, ascending=False).reset_index(drop=True)
-    )
-    
+    df = pd.concat([labels, df.drop(columns=TEMP_FIELD)], axis=1)
+
     return df
 
 
-# df_files
-def cli_df_files(root):
-    '''
-    Scan root directory and return dataframe of file information.
-    '''  
-    assert is_ipython() is False, "[ERROR] This CLI does not support IPython." 
+################################################################
+# encode labels
+################################################################
+def encode_labels(
+    labels: Union[list, np.ndarray],
+    problem: str = 'multi-class',
+    sep: str = '|',
+    return_df=False
+):
+    """Encode labels
 
-    # rich console
-    console = Console()
-    
-    # take a snapshot
-    snapshot = DirectorySnapshot(root)
-    files = sorted([fp for fp in snapshot.paths if os.path.isfile(fp)])
+    Return coded labels, encoder, and decoder.
 
-    # select extensions and filter
-    extensions = _user_select_extensions()
-    files = _filter_files(files, extensions=extensions)
-            
-    # check whether meta is included
-    include_meta = inquirer.confirm(
-        message="Include metadata? (size, modified time):",
-        default=False
-    ).execute()
-           
-    # select subdirs and filter
-    subdirs = _user_select_subdirs(files)
-        
-    # set column names
-    subdirs = sorted({os.path.dirname(x) for x in files})
-    hrchy = _user_set_hrchy(subdirs)
-    
-    # set filepath to save
-    dst_path = inquirer.text(
-        message="Enter path to save ('csv', 'xlsx' are only supported):",
-    ).execute()
-    
-    # create table
-    df = df_files(snapshot=snapshot, hrchy=hrchy, subdirs=subdirs, extensions=extensions, include_meta=include_meta)
-    
-    # preview results
-    _cli_preview_table(df, title=f"\n[Result] Results (total {len(df)} rows)", console=console)
-    print('')
-        
-    # save df
-    if inquirer.confirm(
-            message=f"Save '{dst_path}'?:",
-            default=False
-        ).execute():
-        
-        if dst_path.lower().endswith('.csv'):
-            df.to_csv(dst_path, index=False)
-            return
+    Examples:
+        >>> # multi-class problem
+        >>> labels = ['OK', 'OK', 'NG1', 'NG2', 'OK']
+        >>> encode_labels(labels)
+        (
+            [0, 0, 1, 2, 0],
+            {'OK': 0, 'NG1': 1, 'NG2': 2},
+            {0: 'OK', 1: 'NG1', 2: 'NG2}
+        )
+        >>> # multi-label problem, a.k.a. one hot encoding
+        >>> labels = ['dog', 'cat', 'dog|cat']
+        >>> encode_labels(labels, problem='multi-label')
+        (
+            [[0, 1], [1, 0], [1, 1]],
+            {'dog': 0, 'cat': 1},
+            {0: 'dog', 1: 'cat'}
+        )
 
-        if dst_path.lower().endswith('.xlsx'):
-            df.to_excel(dst_path, index=False)
-            return
+    Args:
+        labels (list, np.ndarray): List of labels with string elements.
+        output (str, optional): 'multi-label' or 'multi-class'.
+        sep (str, optional): For multi-label only. Default is '|'.
+        return_df (bool, optional): For multi-label only. If true, return DataFrame. Defaults to False.
+
+    Returns:
+        list or np.array: Coded labels. List in list out, array in array out.
+        dict: encoder
+        dict: decoder
+    """
+
+    MULTI_CLASS = ['multiclass', 'multiclasses', 'mc']
+    MULTI_LABEL = ['multilabel', 'multilabels', 'ml']
+
+    # correct parameters
+    problem = (
+        problem
+        .replace('-', '').replace('_', '').replace(' ', '')
+        .lower()
+    )
+
+    # get classes
+    if problem in MULTI_CLASS:
+        classes = mlsorted(set(labels))
+    elif problem in MULTI_LABEL:
+        classes = {labs for item in labels for labs in item.split(sep)}
+    else:
+        raise
+    classes = mlsorted(classes)
+    n_classes = len(classes)
+
+    # generate encoder and decoder
+    encoder = {_class: code for code, _class in enumerate(classes)}
+    decoder = {v: k for k, v in encoder.items()}
+
+    # create coded labels
+    if problem in MULTI_CLASS:
+        coded_labels = [encoder[x] for x in labels]
+    elif problem in MULTI_LABEL:
+        coded_labels = list()
+        for x in labels:
+            labs = [0] * n_classes
+            for lab in x.split(sep):
+                labs[encoder[lab]] = 1
+            coded_labels.append(labs)
+
+    # to numpy or to dataframe
+    if return_df:
+        if problem in MULTI_LABEL:
+            coded_labels = pd.DataFrame(coded_labels, columns=encoder.keys())
+        else:
+            coded_labels = pd.DataFrame({'y': coded_labels}, dtype=np.int32)
+    else:
+        if isinstance(labels, np.ndarray):
+            coded_labels = np.array(coded_labels, dtype=np.int32)
+
+    return coded_labels, encoder, decoder
